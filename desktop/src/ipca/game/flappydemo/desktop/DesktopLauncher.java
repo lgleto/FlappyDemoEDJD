638ebe1da349ae462e0c016cf7f9f93b774aa1f1
Lourenço Gomes <leto.lg@gmail.com>
2017-11-27 16:28:12 +0000

    first commit

---
+++ desktop/src/ipca/game/flappydemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ipca.game.flappydemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ipca.game.flappydemo.FlappyDemoGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyDemoGame(), config);
+	}
+}
